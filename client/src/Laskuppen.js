b199e5cc31cf79d73b07d6cb1f787e533e18dc1b
JaoK <jjaokk@gmail.com>
2021-04-13 18:48:18 +0200

    Make a simple navigation structure with react-router-dom

--- client/src/Laskuppen.js
+++ client/src/Laskuppen.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import Sidebar from './components/Sidebar';
+import ContentTestComponent from './components/ContentTestComponent';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 export default class Laskuppen extends React.Component {
 
     render() {
         return (
-            <div>PAGE</div>)
+            <BrowserRouter>
+                <div className="general-styling">
+                    <div className="sidebar-container">
+                        <Sidebar />
+                    </div>
+                    <div className="content-container">
+                        <Switch>
+                            <Route exact path="/toplist" render={(props) => <ContentTestComponent text="toplist" {...props} />}/>
+                            <Route exact path="/reviews" render={(props) => <ContentTestComponent text="reviews" {...props} />}/>
+                            <Route exact path="/makereview" render={(props) => <ContentTestComponent text="makereview" {...props} />}/>
+                            <Route exact path="/books" render={(props) => <ContentTestComponent text="books" {...props} />}/>
+                            <Route exact path="/faq" render={(props) => <ContentTestComponent text="faq" {...props} />}/>
+                        </Switch>
+                    </div>
+                </div>
+            </BrowserRouter>)
     };
 
 }
---
+++ client/src/components/Sidebar.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
+export default class Sidebar extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            sidebarData:
+                [
+                    {
+                        title: "Topplista",
+                        path: "/toplist",
+                        icon: <FontAwesomeIcon icon={faHome} color='white' size='lg' />,
+                        cName: "nav-text"
+                    },
+                    {
+                        title: "Läs Recensioner",
+                        path: "/reviews",
+                        icon: <FontAwesomeIcon icon={faHome} color='white' size='lg' />,
+                        cName: "nav-text"
+                    },
+                    {
+                        title: "Skriv Recension",
+                        path: "/makereview",
+                        icon: <FontAwesomeIcon icon={faHome} color='white' size='lg' />,
+                        cName: "nav-text"
+                    },
+                    {
+                        title: "Böcker",
+                        path: "/books",
+                        icon: <FontAwesomeIcon icon={faHome} color='white' size='lg' />,
+                        cName: "nav-text"
+                    },
+                    {
+                        title: "FAQ",
+                        path: "/faq",
+                        icon: <FontAwesomeIcon icon={faHome} color='white' size='lg' />,
+                        cName: "nav-text"
+                    }
+                ]
+        };
+    }
+
+    render() {        
+        return (
+            <nav className={'nav-menu'}>
+                <h1 className='logo'> Laskuppen </h1>
+                <ul className="nav-menu-items">
+                    {this.state.sidebarData.map((item, index) => {
+                        return (
+                            <li key={index} className={item.cName}>
+                                <Link to={item.path}>
+                                    {item.icon}
+                                    <span className="nav-menu-item-title">
+                                        {item.title}
+                                    </span>
+                                </Link>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </nav>
+        )
+    }
+}
