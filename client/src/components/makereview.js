c13faa7d873e47a6610acb58138ce17ee1aee2ee
Karl Gunnarsson <karlgunnarsson98@gmail.com>
2021-04-14 15:14:46 +0200

    searchbar component created,David,Anton,karl

--- client/src/components/makereview.js
+++ client/src/components/makereview.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import '../css/makereview.css';
+import SearchBar from './SearchBar'
 
 export default class makereview extends Component {
 
@@ -8,10 +9,16 @@ export default class makereview extends Component {
     render() {
         return (
             <div className = 'rdiv'>
+
+                <SearchBar/>
                 
                 <form> 
                     <p> Författare </p>
                     <input type = 'text' />
+
+                    <p> Läsvärd </p>
+                    <input type = 'radio' value = {true} name = "lasvard" />Ja--
+                    <input type = 'radio' value = {false} name = "lasvard" />Nej
 
                     <p> Sidor </p>
                     <input type = 'number' />
@@ -21,10 +28,12 @@ export default class makereview extends Component {
                     
                     <p> Recension </p>
                     <textarea rows = {10} cols = {70}/>
-                   
-
 
                 </form>
+
+                <img src = "https://pbs.twimg.com/profile_images/1181583065811996673/ylZLdBGL_400x400.jpg" height = {100} width={100} />
+
+                
             </div>
         )
     }
