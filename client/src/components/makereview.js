2e0be33f478e518289393d7764d0458199216abf
Karl Gunnarsson <karlgunnarsson98@gmail.com>
2021-04-14 16:55:04 +0200

    fixed subgrids, Karl, David, Anton

--- client/src/components/makereview.js
+++ client/src/components/makereview.js
@@ -8,32 +8,57 @@ export default class makereview extends Component {
 
     render() {
         return (
-            <div className = 'rdiv'>
 
-                <SearchBar/>
-                
-                <form> 
-                    <p> Författare </p>
-                    <input type = 'text' />
+            <div className = "grid-container">
+            
+                     <div className = "A">
+                        <SearchBar/>
+                    </div>
 
-                    <p> Läsvärd </p>
-                    <input type = 'radio' value = {true} name = "lasvard" />Ja--
-                    <input type = 'radio' value = {false} name = "lasvard" />Nej
+                    <div className="B">
 
-                    <p> Sidor </p>
-                    <input type = 'number' />
+                        <div className ="score">
+                            <p> Betyg </p>
+                            <input type = 'number' max={10} min={0} />
+                        </div>
 
-                    <p> Betyg </p>
-                    <input type = 'number' max={10} min={0} />
+                        <div className ="likeable">
+                            <p> Läsvärd </p>
+                            <input type = 'radio' value = {true} name = "lasvard" />Ja--
+                            <input type = 'radio' value = {false} name = "lasvard" />Nej
+                        </div>
+
                     
+                    </div>
+
+                    <div className ="C">
+
+                        <div className ="title">
+                            <p> Titel </p>
+                            <intut type = 'text' />
+                        </div>
+
+                        <div className ="author">
+                            <p> Författare </p>
+                            <input type = 'text' />
+                        </div>
+
+                        <div className ="pages">
+                            <p> Sidor </p>
+                            <input type = 'number' />
+                        </div>
+
+                        <div className ="pix">
+                            <img src = "https://pbs.twimg.com/profile_images/1181583065811996673/ylZLdBGL_400x400.jpg" height = {100} width={100} />    
+
+                        </div>
+                    </div>
+                    
+                    <div className="D"> 
                     <p> Recension </p>
-                    <textarea rows = {10} cols = {70}/>
-
-                </form>
-
-                <img src = "https://pbs.twimg.com/profile_images/1181583065811996673/ylZLdBGL_400x400.jpg" height = {100} width={100} />
-
-                
+                        <textarea rows = {10} cols = {100}/>
+                        
+                    </div>
             </div>
         )
     }
