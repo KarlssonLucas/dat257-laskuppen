0dec2b1fb8a424845e809710c57cff26ff4b34b2
hshanz <david00gbg@hotmail.com>
2021-04-15 14:20:21 +0200

    css fix

--- client/src/components/makereview.js
+++ client/src/components/makereview.js
@@ -10,61 +10,54 @@ export default class makereview extends Component {
     render() {
         return (
 
-            <div className = "grid-container">
-            
-                     <div className = "A">
-                        <SearchBar/>
+            <div className="content">
+
+
+                <div className="grid-container">
+
+                    <div className="A">
+                        <SearchBar />
                     </div>
 
                     <div className="B">
-
-                        <div className ="score">
+                        <div className="score">
                             <p> Betyg </p>
-                            <input type = 'number' max={10} min={0} />
+                            <input type='number' max={10} min={0} />
                         </div>
-
-                        <div className ="likeable">
+                        <div className="likeable">
                             <p> Läsvärd </p>
-                            <input type = 'radio' value = {true} name = "lasvard" />Ja--
-                            <input type = 'radio' value = {false} name = "lasvard" />Nej
+                            <input type='radio' value={true} name="lasvard" />Ja--
+                            <input type='radio' value={false} name="lasvard" />Nej
                         </div>
-
-                    
                     </div>
-
-                    <div className ="C">
-
-                        <div className ="title">
+                    <div className="C">
+                        <div className="title">
                             <p> Titel </p>
-                            <intut type = 'text' />
+                            <intut type='text' />
                         </div>
-
-                        <div className ="author">
+                        <div className="author">
                             <p> Författare </p>
-                            <input type = 'text' />
+                            <input type='text' />
                         </div>
-
-                        <div className ="pages">
+                        <div className="pages">
                             <p> Sidor </p>
-                            <input type = 'number' />
+                            <input type='number' />
                         </div>
-
-                        <div className ="pix">
-                            <img src = "https://pbs.twimg.com/profile_images/1181583065811996673/ylZLdBGL_400x400.jpg" height = {100} width={100} />    
+                        <div className="pix">
+                            <img src="https://pbs.twimg.com/profile_images/1181583065811996673/ylZLdBGL_400x400.jpg" height={100} width={100} />
 
                         </div>
                     </div>
-                    
-                    <div className="D"> 
+                    <div className="D">
                         <p> Recension </p>
-                            <textarea rows = {10} cols = {100}/>
-                        
-                    </div>
+                        <textarea rows={10} cols={100} />
 
+                    </div>
                     <div className="E">
-                        <button className="btn btn-success" onClick={() => {console.log("submitted")}}>Skicka
+                        <button className="btn btn-success" onClick={() => { console.log("submitted") }}>Skicka
                         </button>
                     </div>
+                </div>
             </div>
         )
     }
