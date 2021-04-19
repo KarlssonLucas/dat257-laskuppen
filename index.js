4f99b97acaf1ae8be9fb23504baff3fbb6430307
JaoK <jjaokk@gmail.com>
2021-04-19 19:03:19 +0200

    added url request

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(express.urlencoded({ extended: false }));
 // Create all API-requests
 app.get('/api/users', db.getUsers)
 app.get('/api/users/:id', db.getUserById)
+app.get('/api/users/:id/points', db.getUserPoints)
 app.get('/api/books/:bookname', db.bookssearch)
 app.post('/api/submitreview',db.submitreview);
 app.get('/api/mostreadbook', db.mostReadBook)
