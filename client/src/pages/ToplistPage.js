2aa4c72d59d91fcd1d295d41a74b494cf63ae078
Viktor Johansson <81356794+EnTeori@users.noreply.github.com>
2021-04-20 12:42:54 +0200

    Continued css for toplist. Need to fix columns in box for toplist

--- client/src/pages/ToplistPage.js
+++ client/src/pages/ToplistPage.js
@@ -15,7 +15,7 @@ export default class ToplistPage extends React.Component {
                         <div className="top-reader-card">
                             <p className="card-title"> Veckans toppläsare </p>
                             <hr />
-                            <p className="card-text"> Borges </p>
+                            <p className="card-text"> J.L Borges </p>
                         </div>
 
                         <div className="top-book-card">
@@ -34,17 +34,53 @@ export default class ToplistPage extends React.Component {
                         <div className="my-recommendation">
                             <p className="card-title"> Rekommendation </p>
                             <hr />
-                            <p className="card-text"> Alefen </p>
+                            <p className="card-text"> Alefen (Ljudbok) </p>
                         </div>
 
                         <div className="top-lists">
 
                             <div className="top-list-top-part">
-                                <p > Topplistor: </p>
+
+                                <div className="tltp-header">
+                                    <p > Topplistor: </p>
+                                </div>
+
+                                <div className="tltp-names">
+                                    <p > Namn (Person/Hero): </p>
+                                </div>
+
+                                <div className="tltp-classes">
+                                    <p > Klasser (Lag): </p>
+                                </div>
+
+                                <div className="tltp-points">
+                                    <p > Poäng (XP): </p>
+                                </div>
+
+                                <div className="tltp-read-books">
+                                    <p > Böcker (Arkiv): </p>
+                                </div>
                             </div>
 
                             <div className="top-list-bottom-part">
                                 <p>Elev, klass etc</p>
+
+                                <div className="tlbp-names">
+                                    <p > James Joyce: </p>
+                                </div>
+
+                                <div className="tlbp-classes">
+                                    <p > 2B: </p>
+                                </div>
+
+                                <div className="tlbp-points">
+                                    <p > 4200: </p>
+                                </div>
+
+                                <div className="tlbp-read-books">
+                                    <p > 42: </p>
+                                </div>
+
                             </div>
                         </div>
 
