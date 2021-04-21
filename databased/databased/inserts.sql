e4c9006ddf2973f5379350a1e9f3abf80e8ff47e
Lucas Karlsson <38735934+KarlssonLucas@users.noreply.github.com>
2021-04-21 16:27:36 +0200

    implemented recommended book to a user

---
+++ databased/databased/inserts.sql
@@ -0,0 +1,30 @@
+INSERT INTO School VALUES(DEFAULT,'Chalmers'); -- schoolId = 1
+
+INSERT INTO Classes VALUES
+(DEFAULT,'IT',1),
+(DEFAULT, 'TE2A', 1);
+
+INSERT INTO Roles VALUES
+(1,'Student'),
+(2,'Teacher'), 
+(3,'Admin');
+
+INSERT INTO Users VALUES
+(DEFAULT,'teacher@gmail.com','Linda','Efson','qwe123',1,3),
+(DEFAULT,'jjaokk@gmail.com','Joakim','Ohlsson','qwe123',1,2),
+(DEFAULT,'foo@gmail.com','Foo','Fooson','qwe123',1,1),
+(DEFAULT,'bar@gmail.com','Bar','Barson','qwe123',1,1);
+
+INSERT INTO NewReview (title,author,pages,apiLink,descr,thumbnail,writtenBy,worthReading,rating,summary) VALUES
+('Nice book', '["Apa"]', 100, 'google', 'desc', 'https://images-na.ssl-images-amazon.com/images/I/41joxA5gtjL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg', 1, true, 10, 'brabok1'),
+('Nice book', '["Apa"]', 100, 'google', 'desc', 'https://images-na.ssl-images-amazon.com/images/I/41joxA5gtjL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg', 2, true, 10, 'brabok1'),
+('You liked nice book', '["Apa"]', 100, 'google', 'desc', 'https://images-na.ssl-images-amazon.com/images/I/41joxA5gtjL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg', 2, true, 10, 'brabok1'),
+('Dont catch this ples', '["Apa"]', 100, 'google', 'desc', 'https://images-na.ssl-images-amazon.com/images/I/41joxA5gtjL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg', 3, true, 10, 'brabok1');
+
+INSERT INTO ExtraPoints VALUES 
+(2, 20, 'asdasd'),
+(1, 10, 'asd');
+
+INSERT INTO RecommendedBooks VALUES
+(1, 1),
+(1, 3);
---
+++ databased/databased/views.sql
@@ -0,0 +1,55 @@
+CREATE OR REPLACE VIEW studentsPoints AS
+SELECT Users.id as uidd, SUM(COALESCE(pages,0)) as points, 
+COUNT(Review.id) as booksRead
+FROM Review 
+LEFT JOIN Users ON writtenBy = users.id 
+LEFT JOIN Book ON Book.id = bookId WHERE accepted = true 
+GROUP BY users.id;
+
+-- toplistan filtrera genom att lägga till order by i slutet
+CREATE OR REPLACE VIEW toplistStudent AS 
+SELECT firstName || ' ' || lastName as name, 
+className, COALESCE(studentsPoints.points, 0)+COALESCE(exPoints,0) as points, COALESCE(booksRead, 0) as booksRead
+FROM Users LEFT JOIN studentsPoints ON uidd = id 
+LEFT JOIN ExtraPoints ON userId = id 
+LEFT JOIN Classes ON Classes.id = classId
+WHERE studentsPoints.points > 0 OR exPoints > 0;
+
+CREATE OR REPLACE VIEW allStudPoints AS 
+SELECT Users.id, 
+className, COALESCE(studentsPoints.points, 0)+COALESCE(exPoints,0) as points, COALESCE(booksRead, 0) as booksRead
+FROM Users LEFT JOIN studentsPoints ON uidd = id 
+LEFT JOIN ExtraPoints ON userId = id 
+LEFT JOIN Classes ON Classes.id = classId;
+
+CREATE OR REPLACE VIEW acceptedReviews AS
+SELECT title, firstName || ' ' || lastName as name, author, 
+rating || '/' || '10' as grade, worthReading, summary, accepted, published
+FROM Review LEFT JOIN Users on review.id = users.id
+LEFT JOIN Book ON Book.id = bookId; --add where accepted = true for "läs recensioner"
+
+-- hitta böcker
+CREATE OR REPLACE VIEW booksRead AS
+SELECT title, author, pages, ROUND(AVG(rating)) || '/' || 10 as grade,
+descr, thumbnail
+FROM Review LEFT JOIN Book ON review.id = bookId
+GROUP BY book.id;
+
+-- topplista över mest lästa böckerna
+CREATE OR REPLACE VIEW mostReadBooksToplist AS
+SELECT Book.title, COUNT (Review.bookId) AS freq
+FROM Book LEFT JOIN Review ON Review.bookId = Book.id
+GROUP BY Book.title
+ORDER BY freq DESC; -- filtrera genom att lägga till order by freq DESC/ASC i slutet om vi vill, ASC känns dock irrelevant.
+
+--topplista över högst rankade böckerna
+CREATE OR REPLACE VIEW bestRatedBooksToplist AS
+SELECT Book.title, COALESCE(AVG(rating), 0) AS averageRating
+FROM Book LEFT JOIN Review ON Review.bookId = Book.id
+GROUP BY Book.title
+ORDER BY averageRating DESC;
+
+-- View till inserts för att skapa nya reviews
+CREATE OR REPLACE VIEW NewReview AS 
+SELECT title,author,pages,apiLink,descr,thumbnail,writtenBy,worthReading,rating,summary
+FROM Book RIGHT JOIN Review ON Book.id = Review.bookId;
