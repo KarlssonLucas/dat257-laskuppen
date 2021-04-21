b8174af934eec164b5709d3534ff7e05c4291a76
Lucas Karlsson <38735934+KarlssonLucas@users.noreply.github.com>
2021-04-21 13:28:43 +0200

    fixed problem in database

--- databased/views.sql
+++ databased/views.sql
@@ -6,23 +6,6 @@ LEFT JOIN Users ON writtenBy = users.id
 LEFT JOIN Book ON Book.id = bookId WHERE accepted = true 
 GROUP BY users.id;
 
-CREATE OR REPLACE VIEW recommendedBooks AS
-    WITH test1 AS(
-        SELECT bookid, writtenby, rating, title
-        from review LEFT JOIN Book on
-        bookid=book.id where writtenby=1),
-
-        test2 AS(SELECT bookid, writtenby,
-        rating, title from review left join
-        book on bookid=book.id
-        where writtenby != 1)
-
-   SELECT test2.bookid, test2.writtenby, test2.
-   rating, test2.title FROM test1 JOIN test2
-   ON test1.bookid = test2.bookid;
-
- SELECT * FROM review WHERE writtenBy IN (SELECT writtenBy FROM ASD) AND bookid NOT IN (SELECT bookid FROM asd ORDER BY RATING DESC LIMIT 5);
-
 -- toplistan filtrera genom att lägga till order by i slutet
 CREATE OR REPLACE VIEW toplistStudent AS 
 SELECT firstName || ' ' || lastName as name, 
