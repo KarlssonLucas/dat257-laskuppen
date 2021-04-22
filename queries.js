7e433c2f34587aef739b6572533db946dc026e5d
JaoK <jjaokk@gmail.com>
2021-04-22 14:04:41 +0200

    Update queries.js

--- queries.js
+++ queries.js
@@ -226,7 +226,7 @@ const getRandomRecommendation = (request, response) => {
 
   client.query("WITH test1 AS(SELECT bookid, writtenby, rating, title from review LEFT JOIN Book on bookid=book.id where writtenby=$1), test2 AS(SELECT bookid, writtenby, rating, title from review left join book on bookid=book.id where writtenby != $1), test3 AS (SELECT test2.bookid, test2.writtenby, test2.rating, test2.title FROM test1 JOIN test2 ON test1.bookid = test2.bookid),test4 AS ((SELECT bookid FROM review WHERE writtenBy IN (SELECT writtenBy FROM test3) AND bookid NOT IN (SELECT bookid FROM test3 ORDER BY RATING DESC LIMIT 5)) UNION (SELECT bookid from recommendedBooks)) SELECT id, title, author, descr, pages, thumbnail FROM test4 JOIN book ON id = bookid ORDER BY RANDOM() LIMIT 1", [id], (error, results) => {
     if (error) {
-      throw error;
+      response.status(500).send(errorMsg("Internal server error"));
     } else {
       response.status(200).json(results.rows);
     }
