98ab608c18d5b6c980fd29fc2bd8cac1dfb25e8d
Lucas Karlsson <38735934+KarlssonLucas@users.noreply.github.com>
2021-05-04 18:06:13 +0200

    Started working on the detailview connected to a book
    Started working on the detailview connected to a book

---
+++ client/src/components/BooksPageDetailsComponent.js
@@ -0,0 +1,71 @@
+import './css/bookspagedetails.css';
+import React, { useState, useEffect } from 'react';
+import { render } from 'react-dom';
+
+const BooksPageDetailsComponent = (props) => {
+    const [book, setBook] = useState({});
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(() => {
+        fetchBook();
+        fetchReviews();
+    }, []);
+
+    const fetchBook = async (str) => {
+    const requestOptions = {
+         method: 'GET',
+         headers: { 'Content-Type': 'application/json' },
+     };
+     
+    await fetch("/api/getbook/"+props.id, requestOptions).then(response => response.json()).then(response => {
+        setBook(response[0])
+        });
+    } 
+
+    const fetchReviews = async (str) => {
+        const requestOptions = {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+        };
+    
+        await fetch("/api/reviews/"+props.id, requestOptions).then(response => response.json()).then(response => {
+            setReviews(response);
+        });
+    }
+
+    const reviewsToBook = (reviews) => {
+        return (
+            <div>
+                {reviews.map((review) => {
+                    return (
+                        <div className="">
+                            <div className="">reviewid: {review.rid}</div>
+                            <div className="">review: {review.summary}</div>
+                        </div>
+                    )
+                })}
+            </div>
+        )
+    }
+        
+
+    return (
+        <div className="bpd-page-content">
+            <div className="bookdetails">
+            </div>
+            <div className="bpd-header">
+                <div className="div1">Titel: {book.title}</div>
+                <div className="div2">Författare: {book.author}</div>
+                <div className="div3">Sidantal: {book.pages}</div>
+                <div className="div4">
+                    <img className="bpd-img" src={book.thumbnail} />
+                </div>
+            </div>
+            <div className="bpd-review-result">
+                {reviewsToBook(reviews)}
+            </div>
+        </div>
+    )
+}
+
+export default BooksPageDetailsComponent;
